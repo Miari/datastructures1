0523f71e170f1554fcac5a8ab1aed3b51ecbc7c7
miari <maryna.boroday@gmail.com>
2019-09-03 10:47:05 +0200

    First commit of LL, AL, HM

--- LL, AL, HM/src/datastructures/map/HashMap.java
+++ LL, AL, HM/src/datastructures/map/HashMap.java
@@ -4,21 +4,29 @@ import java.util.*;
 
 public class HashMap<K, V> implements Map<K, V> {
 
-    private static final int INITIAL_CAPACITY = 5;
+    //private static final int INITIAL_CAPACITY = 5;
+    private static int initialCapacity = 5;
     private static final int GROWTH_FACTOR = 2;
-    private static final double LOAD_FACTOR = 0.75;
+    //private static final double LOAD_FACTOR = 0.75;
+    private static double loadFactor = 0.75;
 
-    private int newCapacity = INITIAL_CAPACITY; //не могу сделать её локальной переменной метода rebuild, т.к. она испрользуется также в методе getIndex (где я не могу использовать buckets.length, потому что он вызывается митодом add для ArrayList<Entry>[] newBuckets)
-
-    private ArrayList<Entry<K, V>>[] buckets = new ArrayList[INITIAL_CAPACITY];
+    private ArrayList<Entry<K, V>>[] buckets = new ArrayList[initialCapacity];
     private int size;
+
+    public HashMap() {
+    }
+
+    public HashMap(int initialCapacity, double loadFactor) {
+        this.initialCapacity = initialCapacity;
+        this.loadFactor = loadFactor;
+    }
 
     @Override
     public V put(K key, V value) {
         Entry<K, V> result = getEntry(key);
         if (result == null) {
             add(key, value);
-            if (size > buckets.length * LOAD_FACTOR) {
+            if (size > buckets.length * loadFactor) {
                 rebalance();
             }
             return null;
@@ -40,7 +48,7 @@ public class HashMap<K, V> implements Map<K, V> {
         Entry<K, V> result = getEntry(key);
         if (result == null) {
             add(key, value);
-            if (size > buckets.length * LOAD_FACTOR) {
+            if (size > buckets.length * loadFactor) {
                 rebalance();
             }
             return null;
@@ -89,13 +97,13 @@ public class HashMap<K, V> implements Map<K, V> {
         return size;
     }
 
-    public String toString() {
+    /*public String toString() {
         StringJoiner result = new StringJoiner("; ");
         for (Entry<K, V> current : this) {
-            result.add(String.valueOf(current.key + ": " + current.value));
+            result.add(current.key + ": " + current.value);
         }
         return result.toString();
-    }
+    }*/
 
     public static class Entry<K, V> {
         private K key;
@@ -116,33 +124,37 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     private int getIndex(K key) {
+        return getIndex(key, buckets);
+    }
+
+    private int getIndex(K key, ArrayList[] buckets) {
         if (key == null) {
             return 0;
         }
-        return Math.abs(key.hashCode() % newCapacity);
+        return Math.abs(key.hashCode() % buckets.length);
     }
 
     private void add(K key, V value) {
-        int index = getIndex(key);
-        if (buckets[index] == null) {
-            buckets[index] = new ArrayList<>();
-        }
-        buckets[index].add(new Entry<>(key, value));
+        add(key, value, buckets);
         size++;
     }
 
     private void add(K key, V value, ArrayList<Entry<K, V>>[] newBuckets) {
-        int index = getIndex(key);
+        int index = getIndex(key, newBuckets);
         if (newBuckets[index] == null) {
             newBuckets[index] = new ArrayList<>();
         }
+        // Отвечаю на вопрос, почему здесь Entry просто добавляется в конец, без проверкт, существует ли этот эдемент.
+        // Этот метод у меня именно на добавление. Он используется исключиьтельно для того, чтобы добавить Entry.
+        // Соответственно, если нужна проверка, существеует ли уже Entry в bucket.е, она осуществляется в другом методе с помощью метода getEntry(key), а метод add вызывается в последствии, только в том случае, если Entry не найдена.
+        // Например, это методы put и putIfAbsent. Там я сначала ишу Entry с помощью метода getEntry(key) и только если получаю null, т.е. такой Entry нет, то вызваю метод add.
         newBuckets[index].add(new Entry<>(key, value));
     }
 
     private Entry<K, V> getEntry(K key) {
         int index = getIndex(key);
         if (buckets[index] != null) {
-            for (Entry current : buckets[index]) {
+            for (Entry<K, V> current : buckets[index]) {
                 if (Objects.equals(current.key, key)) {
                     return current;
                 }
@@ -152,8 +164,7 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     private void rebalance() {
-        newCapacity = buckets.length * GROWTH_FACTOR;
-        ArrayList<Entry<K, V>>[] newBuckets = new ArrayList[newCapacity];
+        ArrayList<Entry<K, V>>[] newBuckets = new ArrayList[buckets.length * GROWTH_FACTOR];
         for (Entry<K, V> entry : this) {
             add(entry.key, entry.value, newBuckets);
         }
@@ -166,7 +177,7 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     private class MyIterator implements Iterator<Entry<K, V>> {
-        int bucketIndex;
+        private int bucketIndex;
 
         List<Entry<K, V>> bucket = buckets[bucketIndex];
         Iterator<Entry<K, V>> bucketIterator = bucket.iterator();
@@ -197,6 +208,7 @@ public class HashMap<K, V> implements Map<K, V> {
         @Override
         public void remove() {
             bucketIterator.remove();
+            size--;
         }
     }
 }
--- LL, AL, HM/test/testdatastructures/Map/HashMapTest.java
+++ LL, AL, HM/test/testdatastructures/Map/HashMapTest.java
@@ -7,120 +7,120 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
-import java.util.StringJoiner;
 
 import static org.junit.Assert.*;
 
 public class HashMapTest {
-    private Map map = new HashMap();
-    private Map map1 = new HashMap();
+    private Map <Object, String> firstMap = new HashMap<>(3, 0.6);
+    private Map <Object, String> secondMap = new HashMap<>(3, 0.6);
 
     @Before
     public void fillMap() {
-        map.put("user", "value1");
-        map.put("user", "value2");
-        map.put("user1", "value3");
-        map.put("user1", "value4");
-        map.put("user2", null);
-        map.put("user2", "value5");
-        map.put("user2", null);
-        map.put(null, "value6");
+        firstMap.put("user", "value1");
+        firstMap.put("user", "value2");
+        firstMap.put("user1", "value3");
+        firstMap.put("user1", "value4");
+        firstMap.put("user2", null);
+        firstMap.put("user2", "value5");
+        firstMap.put("user2", null);
+        firstMap.put(null, "value6");
         //new bucket
-        map.put(null, "value7");
-        map.put(1, "value8");
-        map.put(0, "value9");
-        map.put(-1, "value10");
-        map1.put("user", "value21");
-        map1.put("123", "!!!");
-        map1.put("456", "???");
+        firstMap.put(null, "value7");
+        firstMap.put(1, "value8");
+        firstMap.put(0, "value9");
+        firstMap.put(-1, "value10");
+
+        secondMap.put(10, "value21");
+        secondMap.put(123, "!!!");
+        secondMap.put(456, "???");
     }
 
     @Test
     public void testPut() {
-        assertNull(map.put(2, 155));
-        assertEquals(155, map.put(2, "value12"));
+        assertNull(firstMap.put(2, "155"));
+        assertEquals("155", firstMap.put(2, "value12"));
     }
 
     @Test
     public void testPutIfAbsent() {
-        assertNull(map.putIfAbsent(3, "value13"));
-        assertEquals("value13", map.putIfAbsent(3, "value14"));
-        assertEquals("value2", map.putIfAbsent("user", "value15"));
+        assertNull(firstMap.putIfAbsent(3, "value13"));
+        assertEquals("value13", firstMap.putIfAbsent(3, "value14"));
+        assertEquals("value2", firstMap.putIfAbsent("user", "value15"));
     }
 
     @Test
     public void testGet() {
-        assertEquals("value2", map.get("user"));
-        assertEquals("value7", map.get(null));
-        assertNull(map.get("user2"));
-        assertEquals("value9", map.get(0));
-        assertEquals("value10", map.get(-1));
+        assertEquals("value2", firstMap.get("user"));
+        assertEquals("value7", firstMap.get(null));
+        assertNull(firstMap.get(12));
+        assertEquals("value9", firstMap.get(0));
+        assertEquals("value10", firstMap.get(-1));
     }
 
     @Test
+    // переделала тест PutAll без использования метода "toString" но не так, как ты делал на ревью. Там ты удалял по ключу, а у меня этот тест совсем не на удаление.
+    // тут я проверяю метода putAll, который берёт содержимое одной мапы и полностью копирует его в другую, заменяя при этом значения, если запись с таким ключом уже есть в той мапе, в которую копируют.
+    // в итоге, я проверяю, что все знаения из secondMap были скопированы в firstMap, и то, что у скопированных записей значения в первой и второй мапе равны.
+    // а на удаление у меня есть отдельный тест testRemove, он изначально сделан без toString и null передаётся там как знаение, а не как строка
+
     public void testPutAll() {
-        map.putAll(map1);
-        StringJoiner string = new StringJoiner(", ");
-        for (Object entry : map) {
-            string.add(String.valueOf((((HashMap.Entry) entry).getValue())));
+        firstMap.putAll(secondMap);
+        for (HashMap.Entry <Object, String> entry : secondMap) {
+            Object key = entry.getKey();
+            assertTrue(firstMap.containsKey(key));
+            assertEquals(firstMap.get(key), secondMap.get(key));
         }
-        assertEquals("value7, value9, value8, value10, value4, value21, null, ???, !!!", string.toString());
     }
 
     @Test
     public void testPutAllIfAbsent() {
-        map.putAllIfAbsent(map1);
-        StringJoiner string = new StringJoiner(", ");
-        for (Object entry : map) {
-            string.add(String.valueOf((((HashMap.Entry) entry).getValue())));
-        }
-        assertEquals("value7, value9, value8, value10, value4, value2, null, ???, !!!", string.toString());
+        firstMap.putAllIfAbsent(secondMap);
+        assertTrue(firstMap.containsKey(123));
+        assertEquals("!!!", firstMap.get(123));
+        assertTrue(firstMap.containsKey(456));
+        assertEquals("???", firstMap.get(456));
+        assertTrue(firstMap.containsKey("user"));
+        assertEquals("value2", firstMap.get("user"));
     }
 
     @Test
     public void testRemove() {
-        assertEquals("value2", map.remove("user"));
-        assertNull(map.remove("user"));
-        assertEquals("value7", map.remove(null));
-        assertNull(map.remove("user2"));
-        assertEquals("value9", map.remove(0));
-        assertEquals("value10", map.remove(-1));
+        assertEquals("value2", firstMap.remove("user"));
+        assertNull(firstMap.remove(101));
+        assertEquals("value7", firstMap.remove(null));
+        assertNull(firstMap.remove("user2"));
+        assertEquals("value9", firstMap.remove(0));
+        assertEquals("value10", firstMap.remove(-1));
     }
 
     @Test
     public void testContainsKey() {
-        assertTrue(map.containsKey(1));
-        assertFalse(map.containsKey(4));
+        assertTrue(firstMap.containsKey(1));
+        assertFalse(firstMap.containsKey(4));
     }
 
     @Test
     public void testSize() {
-        assertEquals(7, map.size());
-        map.put(4, "value16");
-        assertEquals(8, map.size());
+        assertEquals(7, firstMap.size());
+        firstMap.put(4, "value16");
+        assertEquals(8, firstMap.size());
     }
 
     @Test
     public void testForEach() {
-        StringJoiner string = new StringJoiner(", ");
-        for (Object entry : map) {
-            string.add(String.valueOf((((HashMap.Entry) entry).getValue())));
+        for (HashMap.Entry <Object, String> entry: firstMap) {
+            Object key = entry.getKey();
+            assertTrue(firstMap.containsKey(key));
         }
-        assertEquals("value7, value9, value8, value10, value4, value2, null", string.toString());
     }
 
     @Test
     public void testIteratorRemove() {
-        for (Iterator iterator = map.iterator(); iterator.hasNext(); ) {
+        for (Iterator iterator = firstMap.iterator(); iterator.hasNext(); ) {
             iterator.next();
             iterator.remove();
         }
-        assertEquals("", map.toString());
-    }
-
-    @Test
-    public void testToString() {
-        assertEquals("null: value7; 0: value9; 1: value8; -1: value10; user1: value4; user: value2; user2: null", map.toString());
+        assertEquals(0, firstMap.size());
     }
 }
 
